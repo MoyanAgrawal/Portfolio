9338e9132dec19f5c7c7950058b455b97d16939c
Moyan Agrawal <moyanagrawal144@gmail.com>
2024-07-20 02:14:02 +0530

    final commit

---
+++ pages/api/send-email.js
@@ -0,0 +1,32 @@
+import nodemailer from "nodemailer";
+
+export default async function handler(req, res) {
+  if (req.method === "POST") {
+    const { firstName,lastName, email, message } = req.body;
+
+    // Create a transporter
+    const transporter = nodemailer.createTransport({
+      service: "gmail", // Use your email service
+      auth: {
+        user: process.env.EMAIL_USER, // Your email address
+        pass: process.env.EMAIL_PASS, // Your email password
+      },
+    });
+
+    try {
+      // Send email
+      await transporter.sendMail({
+        from: email,
+        to: process.env.EMAIL_USER, // Your email address
+        subject: `Query message from ${firstName+" "+lastName} from your Porfolio Website!!`,
+        text: message,
+      });
+
+      res.status(200).json({ success: true });
+    } catch (error) {
+      res.status(500).json({ error: "Failed to send email" });
+    }
+  } else {
+    res.status(405).json({ error: "Method not allowed" });
+  }
+}
